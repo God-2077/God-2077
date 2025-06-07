fbd3a11b543eec3f1ac080878e27cdb49d68cd75
Kissablecho <god_2077@outlook.com>
2025-06-08 03:04:29 +0800

    Update system_info.py

--- system_info.py
+++ system_info.py
@@ -1,5 +1,7 @@
 import sys
 import platform
+sys.stdout.reconfigure(encoding='utf-8')  # Python 3.7+
+sys.stderr.reconfigure(encoding='utf-8')
 print("="*50)
 print(f"操作系统: {platform.system()}")
 print(f"架构信息: {platform.machine()}")
